2c24708c44d33536a84a21cec8e601cf896eb4e4
isidora187 <isilp18@gmail.com>
2025-02-17 16:18:45 +0100

    updates menu and prices

--- src/ARFlooringVisualizer.js
+++ src/ARFlooringVisualizer.js
@@ -16,24 +16,34 @@ const images = [
 
 const flooringOptions = [
   { name: "LVT/SPC Vinil",image: "/1304-BONEGA_caa6k6.png", code: "PRK100", price: "21.99 € / m2", glb: "/laminat-1.glb", usdz: "/laminat-1.usdz",link:"/spc-vinil-talne-obloge/" },
-  { name: "Patricia Laminat",image: "/Patricia1 (1).png", code: "PRK100", price: "8.51 €", glb: "/laminat-2.glb", usdz: "/laminat-2.usdz",link:"/patricia/" },
-  { name: "Amalfi Dvoslojni Parket",image: "/BGP_GAIA_vesuvio_AMALFI_rr9snq.png", code: "PRK100", price: "8.51 €", glb: "/laminat-3.glb", usdz: "/laminat-3.usdz",link:"/amalfi-dvoslojni-parket-2/" },
-  { name: "Capri Dvoslojni Parket",image: "/BGP_GAIA_vesuvio_CAPRI_ojlezq.png", code: "PRK100", price: "8.51 €", glb: "/parket-1.glb", usdz: "/parket-1.usdz",link:"/capri-dvoslojni-parket/" },
-  { name: "Kant-Tehno parket",image: "/kant-ali-industrijski-parket-1.jpg", code: "PRK100", price: "8.51 €", glb: "/parket-2.glb", usdz: "/parket-2.usdz",link:"/kant-tehno-parket/" },
-  { name: "LVT/SPC Vinil Akra",image: "/akra-vinilspc.png", code: "PRK100", price: "8.51 €", glb: "/parket-3.glb", usdz: "/parket-3.usdz",link:"/akra/" },
-  { name: "Parket Ribja Kost",image: "/GAIA_fiordo_doga_zvyfrh.jpg", code: "PRK100", price: "8.51 €", glb: "/parket-4.glb", usdz: "/parket-4.usdz",link:"/akra/" },
-  { name: "LVT/SPC Vinil Platano",image: "/Vinil Platano.png", code: "PRK100", price: "8.51 €", glb: "/parket-5.glb", usdz: "/parket-5.usdz" ,link:"/platano/"},
+  { name: "Patricia Laminat",image: "/Patricia1 (1).png", code: "PRK100", price: "8.51 € / m2€", glb: "/laminat-2.glb", usdz: "/laminat-2.usdz",link:"/patricia/" },
+  { name: "Amalfi Dvoslojni Parket",image: "/BGP_GAIA_vesuvio_AMALFI_rr9snq.jpg", code: "PRK100", price: " 39 € / m2", glb: "/laminat-3.glb", usdz: "/laminat-3.usdz",link:"/amalfi-dvoslojni-parket-2/" },
+  { name: "Capri Dvoslojni Parket",image: "/BGP_GAIA_vesuvio_CAPRI_ojlezq.jpg", code: "PRK100", price: "39 € / m2", glb: "/parket-1.glb", usdz: "/parket-1.usdz",link:"/capri-dvoslojni-parket/" },
+  { name: "Kant-Tehno parket",image: "/kant-ali-industrijski-parket-1.jpg", code: "PRK100", price: "kontakt za ceno", glb: "/parket-2.glb", usdz: "/parket-2.usdz",link:"/kant-tehno-parket/" },
+  { name: "LVT/SPC Vinil Akra",image: "/akra-vinilspc.png", code: "PRK100", price: "21.99 € / m2", glb: "/parket-3.glb", usdz: "/parket-3.usdz",link:"/akra/" },
+  { name: "Parket Ribja Kost",image: "/GAIA_fiordo_doga_zvyfrh.jpg", code: "PRK100", price: "33.12€ / m2", glb: "/parket-4.glb", usdz: "/parket-4.usdz",link:"/akra/" },
+  { name: "LVT/SPC Vinil Platano",image: "/Vinil Platano.png", code: "PRK100", price: "21.99 € / m2", glb: "/parket-5.glb", usdz: "/parket-5.usdz" ,link:"/platano/"},
   { name: "Talya Laminat",image: "/Talya2 (2).png", code: "PRK100", price: "8.51 €", glb: "/parket-6.glb", usdz: "/parket-6.usdz" ,link:"/talya/"},
   { name: "Querica Antica Dvoslojni Parket",image: "/GAIA_SPINA-ITALIANA_QUERCIA-ANTICA_i9noh3.jpg", code: "PRK100", price: " 52.56 € / m2", glb: "/parket-7.glb", usdz: "/parket-7.usdz",link:"/querica-antica-dvoslojni-parket/" },
   { name: "Dark Parquet",image: "/BGP_GAIA_vesuvio_CAPRI_ojlezq (1).jpg", code: "PRK100", price: "39 € / m2", glb: "/parket-8.glb", usdz: "/parket-8.usdz",link:"/capri-dvoslojni-parket/"},
   { name: "Lugano Laminat",image: "/Lugano2 (2).png", code: "PRK1106", price: "8.51 € / m2", glb: "/parket-9.glb", usdz: "/parket-9.usdz",link:"/lugano/" },
-  { name: "Fiordo dvoslojni parket",image: "/GAIA_fiordo_doga_zvyfrh (1).jpg", code: "PRK100", price: "45.50 € / m2", glb: "/parket-10.glb", usdz: "/parket-10.usdz",link:"/fiordo-dvoslojni-parket/" },
+  { name: "Fiordo dvoslojni parket",image: "/GAIA_fiordo.jpg", code: "PRK100", price: "45.50 € / m2", glb: "/parket-10.glb", usdz: "/parket-10.usdz",link:"/fiordo-dvoslojni-parket/" },
   { name: "Merik Laminat", image: "/Meric2 (1).png", code: "PRK100", price: "8.51 €", glb: "/laminat-1.glb", usdz: "/laminat-1.usdz",link:"/merik/" },
 ];
 
 const ARFlooringVisualizer = () => {
   const [selectedFlooring, setSelectedFlooring] = useState(flooringOptions[0]);
   const [showText, setShowText] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 3 < flooringOptions.length ? prevIndex + 3 : 0));
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 3 >= 0 ? prevIndex - 3 : flooringOptions.length - 3));
+  };
+
 
   useEffect(() => {
     const handleScroll = () => {
@@ -61,19 +71,19 @@ const ARFlooringVisualizer = () => {
       <p className="p-styling">Talne obloge so velika naložba, ki je kupci ne jemljejo zlahka. Preproga, ploščice ali trden les – ne glede na izdelek, za katerega se naša stranka odloči, bistveno je, da dobi pravega.Z našo napredno tehnologijo vizualizacije kupcem pokažemo, kako bodo vaši izdelki izgledali v njihovih domovih, preden dejansko opravijo nakup.</p>
       <p className="p-styling">V meniju izberite tla, ki jih želite preizkusiti, kliknite na pogled v ar in usmerite telefon na tla</p>
       </div>
-      <select
-        onChange={(e) =>
-          setSelectedFlooring(flooringOptions.find((f) => f.name === e.target.value))
-        }
-        value={selectedFlooring.name}
-        className="floor-selector"
-      >
-        {flooringOptions.map((floor) => (
-          <option key={floor.name} value={floor.name}>
-            {floor.name}
-          </option>
-        ))}
-      </select> 
+      {/* Flooring Selection Carousel */}
+      <div className="flooring-selection">
+        <button className="carousel-btn prev" onClick={prevSlide}><i class="arrow left"></i></button>
+        <div className="flooring-carousel">
+          {flooringOptions.slice(currentIndex, currentIndex + 3).map((floor) => (
+            <div key={floor.name} className={`flooring-option ${selectedFlooring.name === floor.name ? "selected" : ""}`} onClick={() => setSelectedFlooring(floor)}>
+              <img src={floor.image} alt={floor.name} className="flooring-image" />
+              <p>{floor.name}</p>
+            </div>
+          ))}
+        </div>
+        <button className="carousel-btn next" onClick={nextSlide}><i class="arrow right"></i></button>
+      </div>
       </div>
 
       {/* AR Model Viewer */}
